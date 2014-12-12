c5b2d7787ff3678142201d500803d9023da483ce
Rastermyosin <kjon.oneill@gmail.com>
2014-12-12 08:44:53 -0700

    Added PowerShell Startup Script
    Added PowerShell Startup Sctript for the mini computer.
    -Kevin

---
+++ PowerShell/OculusStartup.ps1
@@ -0,0 +1,27 @@
+##############################################
+#	Title: Oculus Rift Startup File
+# 	Desc: Starts the Unity/Oculus program at startup for patient 
+#		  volunteers when that use their practice system.
+#	Author: Kevin O'Neill
+#	Date: 2014/12/11
+##############################################
+
+# Write Logo
+Write-Host "";
+Write-Host "**************************";
+Write-Host "*      _   __________    *";
+Write-Host "*     / | / / ____/ /    *";
+Write-Host "*    /  |/ / __/ / /     *";
+Write-Host "*   / /|  / /___/ /___   *";
+Write-Host "*  /_/ |_/_____/_____/   *";
+Write-Host "*                        *";
+Write-Host "* Neural Engineering Lab *";
+Write-Host "**************************";
+Write-Host "";
+
+Start-Sleep -s 5
+
+Write-Host "Test done.";
+
+#cd C:\CodeRepo\
+
